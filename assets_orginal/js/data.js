52306c04da85e8585481bb96771a207476cdbbcb
glennthielman <37749736+glennthielman@users.noreply.github.com>
2025-05-08 19:27:34 +0200

    one json file and removed templating library in favor for vanilla js

--- assets_orginal/js/data.js
+++ assets_orginal/js/data.js
@@ -1,16 +1,56 @@
-function fetchAndRender (name) {
-    fetch(name + '.json')
-        .then(response => response.json())
-        .then(data => {
-            const mysource = document.getElementById(name + '-template').innerHTML;
-            const mytemplate = Handlebars.compile(mysource);
-            const myresult = mytemplate(data);
-            document.getElementById(name).innerHTML = myresult;
-        });
+function renderProviders(providers){
+    let html = ` <table>
+                        <tr>
+                            <th>Website</th>
+                            <th>Prefix</th>
+                        </tr>`;
+    for(let i = 0; i < providers.length; i++) {
+        html += `<tr>
+                    <td><a href="${providers[i].url}">${providers[i].name}</a></td>
+                    <td>${providers[i].prefix}</td>
+                </tr>`;
+    }
+    html += '</table>';
+    document.getElementById("providers").innerHTML = html;  
+}
+
+function renderApps(apps) {
+    let html = '';
+    for(let i = 0; i < apps.length; i++) {
+        html += `<div class="apps_item">
+                    <div class="apps_icon">
+                        <span class="iconify icon" data-icon="mdi-${apps[i].icon}"></span>
+                    </div>
+                    <div class="apps_text">
+                        <a href="http://${apps[i].url}" target="${apps[i].target}" >${apps[i].name}</a>
+                        <span id="app-address">${apps[i].url}</span>
+                    </div>
+                </div>`;
+    }
+    document.getElementById("apps_loop").innerHTML = html;   
+}
+
+function renderLinks(bookmarks) {
+    let html = '';
+    for(let i = 0; i < bookmarks.length; i++) {
+        var links = bookmarks[i].links;
+        html += `<div id="links_item">
+                    <h4>${bookmarks[i].category}</h4>`
+                    for(let j = 0; j < links.length; j++) {
+                        html+= `<a href="${links[j].url}" target="_blank" class="theme_color-border theme_text-select">${links[j].name}</a>`
+                    }    
+        html+= `</div>`;
+    }
+    document.getElementById("links_loop").innerHTML = html;   
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetchAndRender('apps');
-    fetchAndRender('links');
-    fetchAndRender('providers');
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            renderApps(data.apps);
+            renderLinks(data.bookmarks);
+            renderProviders(data.providers);
+        });
 });
